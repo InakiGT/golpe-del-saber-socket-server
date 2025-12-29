3d53ed741005774f68509d99acf0e63fbf2f6fbc
InakiGT <yarimfuego@gmail.com>
2025-12-29 14:42:15 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import config from './config/index.js'
+import bindSockets from './sockets.js'
+
+const app = express()
+const server = createServer(app)
+
+app.use(cors())
+
+const io = new Server(server, {
+  cors: {
+    origin: config.allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+})
+
+bindSockets(io)
+
+app.get('/', (_, res) => {
+  res.json({
+    msg: 'Golpe del saber websocket server is working'
+  })
+})
+
+server.listen(config.port, () => {
+  console.log(`App listening on PORT ${ config.port }`)
+})
---
+++ src/sockets.js
@@ -0,0 +1,91 @@
+
+export default function bindSockets(io) {
+  io.on('connection', (socket) => {
+    socket.on('join-party-host', (data) => {
+      const { partyId } = data
+
+      socket.join(partyId)
+    })
+
+    socket.on('join-party-team', (data) => {
+      const { partyId, teamName, teamId } = data
+      socket.join(partyId)
+
+      io.to(partyId).emit('team-joined', {
+        from: socket.id,
+        payload: {
+          teamName,
+          teamId,
+        }
+      })
+    })
+
+    socket.on('leave-party', (data) => {
+      io.to(data.partyId).emit('finish-game')
+    })
+
+    socket.on('attacking-time', (data) => {
+      io.to(data.partyId).emit('attacking-time', {
+        from: socket.id,
+        payload: {
+          attacking: true,
+        },
+      })
+    })
+
+    socket.on('attack-message', (data) => {
+      const { partyId, attackingTeam, attackedTeam } = data
+
+      io.to(partyId).emit('attacking-message', {
+        from: socket.id,
+        payload: {
+          attackingTeam,
+          attackedTeam,
+        },
+      })
+    })
+
+    socket.on('answer-message', (data) => {
+      const { partyId, teamId } = data
+
+      io.to(partyId).emit('party-message', {
+        from: socket.id,
+        payload: {
+          teamId,
+        },
+      })
+    })
+
+    socket.on('party-started', (data) => {
+      const { partyId, teams } = data
+
+      io.to(partyId).emit('party-started', {
+        from: socket.id,
+        payload: {
+          teams,
+        }
+      })
+    })
+
+    socket.on('next-question', (data) => {
+      const { partyId, question } = data
+      io.to(partyId).emit('next-question', {
+        from: socket.id,
+        payload: {
+          question,
+        }
+      })
+    });
+
+    socket.on('answering-time', (data) => {
+      const { partyId, answering } = data
+
+      io.to(partyId).emit('change-team-status', {
+        from: socket.id,
+        payload: {
+          answering,
+        }
+      })
+    })
+  })
+}
